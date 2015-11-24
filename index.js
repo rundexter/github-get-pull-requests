7525e3277e9b6ea8d40dd9b6479697aa5bbd270a
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-11-24 15:29:21 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,46 @@
+var GitHubApi = require("github");
+var _ = require('lodash');
+
 module.exports = {
+    /**
+     * Pick API result.
+     *
+     * @param pullRequests
+     * @returns {Array}
+     */
+    pickResultData: function (pullRequests) {
+        var result = [];
+
+        _.map(pullRequests, function (pullRequest, requestKey) {
+
+            if (pullRequest._links) {
+                result.push(pullRequest._links);
+            }
+        });
+
+        return result;
+    },
+
+    /**
+     * Authenticate gitHub user.
+     *
+     * @param dexter
+     * @param github
+     */
+    gitHubAuthenticate: function (dexter, github) {
+
+        if (dexter.environment('GitHubUserName') && dexter.environment('GitHubPassword')) {
+
+            github.authenticate({
+                type: dexter.environment('GitHubType') || "basic",
+                username: dexter.environment('GitHubUserName'),
+                password: dexter.environment('GitHubPassword')
+            });
+        } else {
+            this.fail('A GitHubUserName and GitHubPassword environment variable is required for this module');
+        }
+    },
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +48,26 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+
+        var github = new GitHubApi({
+            // required 
+            version: "3.0.0"
+        });
+
+        this.gitHubAuthenticate(dexter, github);
+
+        if (step.input('owner').first() && step.input('repo').first()) {
+
+            github.pullRequests.getAll({
+                user: step.input('owner').first(),
+                repo: step.input('repo').first()
+            }, function (err, pullRequests) {
+
+                err? this.fail(err) : this.complete(this.pickResultData(pullRequests));
+            }.bind(this));
+        } else {
+
+            this.fail('A owner input variable is required for this module');
+        }
     }
 };
